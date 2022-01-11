6e4eb607b69cbf49bb1bd3b12e81a87db1b2f48b
Finn Catling <finn.catling@gmail.com>
2022-01-11 16:36:50 +0000

    Function to make waveform generator

--- sidewinder/synthetic.py
+++ sidewinder/synthetic.py
@@ -1,1 +1,18 @@
+import numpy as np
+from scipy import interpolate
 
+
+def make_waveform_generator_from_file(filepath: str) -> interpolate.interp1d:
+    """Make scipy interpolation function from saved example waveform file. This
+    function can be used to generate resampled synthetic waveforms.
+
+    Args:
+        filepath: Path to .npy file containing a 1D numpy array of the example
+            waveform
+
+    Returns:
+        Interpolation function
+    """
+    waveform = np.load(filepath)
+    time = np.linspace(start=0, stop=1, num=waveform.size)
+    return interpolate.interp1d(time, waveform, kind='cubic')
--- tests/test_synthetic.py
+++ tests/test_synthetic.py
@@ -1,1 +1,17 @@
+import os
 
+from scipy import interpolate
+
+from sidewinder import utils
+from sidewinder import synthetic
+
+
+def test_make_waveform_generator_from_file():
+    waveform_filepath = os.path.join(
+        utils.get_root_directory(),
+        'sidewinder',
+        'data',
+        'example_arterial_pressure_waveform.npy'
+    )
+    generator = synthetic.make_waveform_generator_from_file(waveform_filepath)
+    assert isinstance(generator, interpolate.interp1d)
