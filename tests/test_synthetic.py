2a4c25f8e35f2a08dce3eb256a1654f29ce0808e
Finn Catling <finn.catling@gmail.com>
2022-01-12 13:37:17 +0000

    Tests for synthetic data generation

--- tests/test_synthetic.py
+++ tests/test_synthetic.py
@@ -1,5 +1,7 @@
 import os
 
+import numpy as np
+import pandas as pd
 from scipy import interpolate
 
 from sidewinder import utils
@@ -15,3 +17,28 @@ def test_make_waveform_generator_from_file():
     )
     generator = synthetic.make_waveform_generator_from_file(waveform_filepath)
     assert isinstance(generator, interpolate.interp1d)
+
+
+def test_make_generator_timestamps_and_inputs():
+    timestamps, inputs = synthetic.make_generator_timestamps_and_inputs(
+        cycles_per_minute=60.,
+        n_cycles_target=2.,
+        hertz=10.
+    )
+    assert (timestamps == np.linspace(0, 2, 20, endpoint=False)).all()
+    assert (inputs == np.tile(np.linspace(0, 1, 10, endpoint=False), 2)).all()
+
+
+def test_synthetic_arterial_pressure_data():
+    art = synthetic.synthetic_arterial_pressure_data(
+        systolic_pressure=120.,
+        diastolic_pressure=80.,
+        heart_rate=60.,
+        n_beats_target=2.,
+        hertz=10.
+    )
+    assert (art.time.values == np.linspace(0, 2, 20, endpoint=False)).all()
+    assert art.pressure.max() == 120.
+    assert art.pressure.min() == 80.
+    assert art.shape == (20, 2)
+    assert isinstance(art, pd.DataFrame)
