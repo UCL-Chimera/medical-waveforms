f86a65e386824c73d0e043119cad9df547a956fa
Finn Catling <finn.catling@gmail.com>
2022-01-21 14:06:04 +0000

    Find troughs in waveform data

---
+++ sidewinder/features.py
@@ -0,0 +1,19 @@
+from pyampd import ampd
+
+from .waveforms import Waveforms
+
+
+def find_troughs(waveforms: Waveforms, name: str):
+    """Finds indices of troughs in a waveform.
+
+    Args:
+        waveforms: `sidewinder.waveforms.Waveforms` instance holding you data
+        name: Name of column in `waveforms` to find troughs in
+
+    Returns:
+        `waveforms` with trough indices added to `waveforms.features[`name`]`
+    """
+    waveforms.waveforms[name] *= -1  # invert signal, so troughs become peaks
+    waveforms.features[name] = ampd.find_peaks(waveforms.waveforms[name])
+    waveforms.waveforms[name] *= -1  # un-invert signal
+    return waveforms
