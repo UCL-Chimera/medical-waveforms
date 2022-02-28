bfc4e0f094030360957fb7183a19c0c197ce34c0
Finn Catling <finn.catling@gmail.com>
2022-02-28 16:26:50 +0000

    Refactor features test using fixtures

--- tests/test_features.py
+++ tests/test_features.py
@@ -1,19 +1,29 @@
+import pytest
 import numpy as np
+import pandas as pd
 
 from sidewinder import synthetic, waveforms
 from sidewinder.features import waveform
 
 
-def test_find_troughs():
-    data = synthetic.synthetic_arterial_pressure_data(
+@pytest.fixture(scope='function')
+def abp_data_fixture() -> pd.DataFrame:
+    return synthetic.synthetic_arterial_pressure_data(
         systolic_pressure=120.,
         diastolic_pressure=80.,
         heart_rate=60.,
         n_beats_target=2.5,
         hertz=10.
     )
-    w = waveforms.Waveforms(data)
-    w = waveform.find_troughs(w, name='pressure')
+
+
+@pytest.fixture(scope='function')
+def abp_waveforms_fixture(abp_data_fixture) -> waveforms.Waveforms:
+    return waveforms.Waveforms(abp_data_fixture)
+
+
+def test_find_troughs(abp_waveforms_fixture):
+    w = waveform.find_troughs(abp_waveforms_fixture, name='pressure')
     np.testing.assert_array_equal(
         w.waveform_features['pressure']['troughs'],
         np.array([0, 10, 20])
