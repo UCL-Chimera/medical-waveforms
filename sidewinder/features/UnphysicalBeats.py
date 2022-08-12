7e72e077be93a5786d2d987dcb97c0c52332853f
Finn Catling <finn.catling@gmail.com>
2022-08-12 17:29:54 +0100

    debug, removed unnecessary variables

--- sidewinder/features/UnphysicalBeats.py
+++ sidewinder/features/UnphysicalBeats.py
@@ -19,14 +19,17 @@ def unphysbeats(
     waveforms: Waveforms,
     cycle_thresholds: Dict[str, Tuple[float, float]] = frozendict(
         {
-            "MinimumValue": (20.0, 300.0),  # mmHg
+            "Value": (20.0, 300.0),  # mmHg
             "MeanValue": (30.0, 200.0),  # mmHg
             "CyclesPerMinute": (20.0, 200.0),  # bpm
             "MaximumMinusMinimumValues": (20.0, np.inf),  # mmHg
         }
     ),
 ):
-    # flag unphysiological beats based on threshold
+    # TODO: Tweak default thresholds
+    # TODO: Separate thresholds for DBP and SBP
+
+    # Flag unphysiological beats based on threshold
     for feature_extractor in [
         cycle.CyclesPerMinute,
         cycle.MaximumValue,
@@ -35,36 +38,27 @@ def unphysbeats(
         cycle.MaximumMinusMinimumValue,
         cycle.MeanNegativeFirstDifference,
     ]:
-        waveforms = feature_extractor().extract_feature(
-            waveforms, "pressure"
-        )  ###Picked up from Finn's example.
-    Pdias = waveforms.cycle_features["MinimumValue"]
-    Psys = waveforms.cycle_features["MaximumValue"]
-    MAP = waveforms.cycle_features["MeanValue"]
-    PP = waveforms.cycle_features["MaximumMinusMinimumValue"]
-    HR = waveforms.cycle_features["CyclesPerMinute"]
+        waveforms = feature_extractor().extract_feature(waveforms, "pressure")
 
-    ##Replace without defining the new variables above, if correct
+    # TODO: Do this is a loop
     badP = np.where(
-        np.any(
-            Pdias < cycle_thresholds["MinimumValue"][0]
-            or Psys > cycle_thresholds["MinimumValue"][1]
-        )
+        cycle_thresholds["Value"][0]
+        > waveforms.cycle_features["pressure"]["MinimumValue"]
+        > cycle_thresholds["Value"][1]
     )
     badMAP = np.where(
-        np.any(
-            MAP < cycle_thresholds["MeanValue"][0]
-            or MAP > cycle_thresholds["MeanValue"][1]
-        )
+        cycle_thresholds["MeanValue"][0]
+        > waveforms.cycle_features["pressure"]["MeanValue"]
+        > cycle_thresholds["MeanValue"][1]
     )
     badHR = np.where(
-        np.any(
-            HR < cycle_thresholds["CyclesPerMinute"][0]
-            or HR > cycle_thresholds["CyclesPerMinute"][1]
-        )
+        cycle_thresholds["CyclesPerMinute"][0]
+        > waveforms.cycle_features["pressure"]["CyclesPerMinute"]
+        > cycle_thresholds["CyclesPerMinute"][1]
     )
     badPP = np.where(
-        np.any(PP < cycle_thresholds["MaximumMinusMinimumValues"][0])
+        waveforms.cycle_features["pressure"]["MaximumMinusMinimumValue"]
+        < cycle_thresholds["MaximumMinusMinimumValues"][0]
     )
 
     ###First differences. Smarter way than differencing each time?
