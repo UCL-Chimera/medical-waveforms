e8208f1b9e80b9bd233a3b8c0d1d41c765393c0c
Finn Catling <finn.catling@gmail.com>
2022-08-16 15:32:19 +0100

    Defined checks with pydantic class

--- sidewinder/quality.py
+++ sidewinder/quality.py
@@ -1,90 +1,100 @@
-from dataclasses import dataclass, field
-from typing import Dict, Tuple, Type
+from typing import Optional, Type
 
+import numpy as np
 import pandas as pd
-# from frozendict import frozendict
+from pydantic import BaseModel, Extra
 
 from sidewinder.features import cycles, diffs
 from sidewinder.waveforms import Waveforms
 
-# ycle_thresholds: Dict[
-#         Type[cycles.CycleFeatureExtractor], Tuple[float, float]
-#     ] = frozendict(
-#         {
-#             cycles.MinimumValue: (20.0, 200.0),  # mmHg
-#             cycles.MaximumValue: (30.0, 300.0),  # mmHg
-#             cycles.MeanValue: (30.0, 200.0),  # mmHg
-#             cycles.CyclesPerMinute: (20.0, 200.0),  # bpm
-#             cycles.MaximumMinusMinimumValue: (20.0, 250.0),  # mmHg
-#             cycles.MeanNegativeFirstDifference: (-3.0, 0.0),
-#         }
-#     ),
-#     diff_thresholds: Dict[
-#         Type[cycles.CycleFeatureExtractor], float
-#     ] = frozendict(
-#         {
-#             cycles.MinimumValue: 20.0,  # mmHg
-#             cycles.MaximumValue: 20.0,  # mmHg
-#             cycles.Duration: 0.5,  # seconds
-#         }
-#     ),
-@dataclass
-class CycleThreshold:
-    # checker: Type[cycles.CycleFeatureExtractor]
-    min: float
-    max: float
+
+class CycleCheck(BaseModel):
+    feature: Type[cycles.CycleFeatureExtractor]
+    min: float = -np.inf
+    max: float = np.inf
+    units: Optional[str] = None
+    description: Optional[str] = None
 
 
-@dataclass
-class DiffThreshold:
-    # checker: Type[cycles.CycleFeatureExtractor]
-    absolute: bool = True
-    threshold: float = 20
+class DiffCheck(BaseModel):
+    feature: Type[cycles.CycleFeatureExtractor]
+    threshold: float
+    units: Optional[str] = None
+    description: Optional[str] = None
 
-@dataclass
-class CheckThresholds:
-    #TODO look at using default_factory() to build in different defaults
-    
-    @dataclass
-    class Cycles:
-        min: CycleThreshold = CycleThreshold(20, 300) 
-        max = CycleThreshold(30, 300) 
-        mean = CycleThreshold(30, 200) 
-        Hz = CycleThreshold(20, 200) 
-        diff = CycleThreshold(20, 250) 
-        nfd = CycleThreshold(-3, 0) 
-    
-    
-    class Thresholds:
-        min: DiffThreshold()
-        max: DiffThreshold()
-        duration: DiffThreshold(threshold=0.5)
 
-# TODO: These are defaults for adult arterial pressure - move them elsewhere
+class ArterialPressureChecks(BaseModel):
+    diastolic_pressure: Optional[CycleCheck] = CycleCheck(
+        feature=cycles.MinimumValue, min=20.0, max=200.0, units="mmHg"
+    )
+    systolic_pressure: Optional[CycleCheck] = CycleCheck(
+        feature=cycles.MaximumValue, min=30.0, max=300.0, units="mmHg"
+    )
+    mean_pressure: Optional[CycleCheck] = CycleCheck(
+        feature=cycles.MeanValue, min=30.0, max=200.0, units="mmHg"
+    )
+    heart_rate: Optional[CycleCheck] = CycleCheck(
+        feature=cycles.CyclesPerMinute,
+        min=20.0,
+        max=200.0,
+        units="beats per minute",
+    )
+    pulse_pressure: Optional[CycleCheck] = CycleCheck(
+        feature=cycles.MaximumMinusMinimumValue,
+        min=20.0,
+        max=250.0,
+        units="mmHg",
+    )
+    mean_dyneg: Optional[CycleCheck] = CycleCheck(
+        feature=cycles.MeanNegativeFirstDifference,
+        min=-3.0,
+        max=0.0,
+        units="mmHg",
+    )
+    diastolic_pressure_diff: Optional[DiffCheck] = DiffCheck(
+        feature=cycles.MinimumValue, threshold=20.0, units="mmHg"
+    )
+    systolic_pressure_diff: Optional[DiffCheck] = DiffCheck(
+        feature=cycles.MaximumValue, threshold=20.0, units="mmHg"
+    )
+    beat_time_diff: Optional[DiffCheck] = DiffCheck(
+        feature=cycles.Duration, threshold=0.5, units="seconds"
+    )
+
+    class Config:
+        """Allow users to define extra checks on instantiation."""
+
+        extra = Extra.allow
+
+
 def check_cycles(
-    waveforms: Waveforms,
-    name: str,
-    cycle_thresholds: CheckThresholds = CheckThresholds()
+    waveforms: Waveforms, name: str, checks: Type[BaseModel]
 ) -> pd.DataFrame:
     checked = {}
 
-    # Flag unphysiological beats
-    for feature_extractor, thresholds in cycle_thresholds.items():
-        fe = feature_extractor()
-        waveforms = fe.extract_feature(waveforms, name)
-        checked[fe.class_name] = (
-            waveforms.features.cycles[name][fe.class_name] > thresholds[0]
-        ) & (waveforms.features.cycles[name][fe.class_name] < thresholds[1])
+    for check_name, check in vars(checks).items():
+        if isinstance(check, CycleCheck):
+            # Flag unphysiological cycles
+            feature_extractor = check.feature()
+            waveforms = check.feature().extract_feature(waveforms, name)
+            checked[check_name] = (
+                waveforms.features.cycles[name][feature_extractor.class_name]
+                > check.min
+            ) & (
+                waveforms.features.cycles[name][feature_extractor.class_name]
+                < check.max
+            )
 
-    # Flag unphysiological beat-to-beat changes
-    for feature_extractor, threshold in diff_thresholds.items():
-        fe = feature_extractor()
-        waveforms = diffs.calculate_absolute_diffs(
-            waveforms, name, feature_extractor
-        )
-        checked[f"{fe.class_name}_diff"] = (
-            waveforms.features.diffs[name][fe.class_name] < threshold
-        )
+        elif isinstance(check, DiffCheck):
+            # Flag unphysiological cycle-to-cycle changes
+            feature_extractor = check.feature()
+            waveforms = diffs.calculate_absolute_diffs(
+                waveforms, name, check.feature
+            )
+            checked[check_name] = (
+                waveforms.features.diffs[name][feature_extractor.class_name]
+                < check.threshold
+            )
 
     checked_df = pd.DataFrame(checked)
     checked_df["all"] = checked_df.all(axis=1)
